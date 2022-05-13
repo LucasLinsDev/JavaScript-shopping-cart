1f494824d379f8ecb93d8daf2084dc2888adb5f4
Lucas Lins <lucaslinsperfil@gmail.com>
2022-05-13 01:30:49 -0300

    add main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('running');
+
+
+//select all add-cart class
+let carts =document.querySelectorAll('.add-cart');
+carts[0]
+carts[1]
+
+for (let i=0; i< carts.length; i++){
+  console.log(i,'my loop')
+}
